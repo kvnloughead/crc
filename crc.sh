9f54f4c2ae0d4cafcaa9e893a032c71a8501c90b
Kevin Loughead <kvnloughead@gmail.com>
2021-11-10 20:38:19 -0500

    updated templates

--- crc.sh
+++ crc.sh
@@ -4,33 +4,33 @@
 #
 # Writes boilerplate for new React component. Use it from inside
 # of your components directory.
-# 
+#
 # USAGE
-#   crc component-name [OPTIONS]  
+#   crc component-name [OPTIONS]
 #
 #
 # POSITIONAL PARAMETERS
 #   component-name
-#       the name of the component       
-#     
-# 
+#       the name of the component
+#
+#
 # OPTIONS
-#   -s|--style 
+#   -s|--style
 #       The type of styling being used. Defaults to vanilla css.
 #       Options - styled-components (sc)
 #
 # DESCRIPTION
 #   Creates a file structure like this inside the cwd
-#     
-#     cwd 
+#
+#     cwd
 #       |____ component-name/
-#                           |______ component-name.jsx
+#                           |______ component-name.js
 #                           |______ styles.css
 #
-#   Fills jsx file with boiler plate found in the crc-templates directory.
-#   If `-s sc` option is set, replaces styles.css with styles.js and 
+#   Fills js file with boiler plate found in the templates directory.
+#   If `-s sc` option is set, replaces styles.css with styles.js and
 #   adds a few necessary import statements for working with styled-components.
-#   
+#
 # TODO
 #   2. add a -m|--modules option, to list options for import.
 #   3. add a -c|--class flag to create class component instead
@@ -41,24 +41,24 @@
 # because getopts doesn't like them to come first
 POS_ARGS=()
 while [ $# -gt 0 ]
-do 
+do
   # if argument starts with `-`, break; otherwise, shift args by 1
   if [[ $1 =~ ^-.* ]]
-    then 
+    then
       break
     else
       # supports additional positional arguments
       POS_ARGS+=($1)
       shift
   fi
-done 
+done
 NAME=${POS_ARGS[0]}
 
 mkdir $NAME
-# redirect text from template into component.jsx
-cat ~/bin/crc/crc-templates/func-component.jsx > $NAME/$NAME.jsx
+# redirect text from template into component.js
+cat ~/bin/crc/templates/func-component.js > $NAME/$NAME.js
 # replace instances of string COMPONENT with the supplied name of component
-sed -i -e 's/COMPONENT/'$NAME'/g' $NAME/$NAME.jsx 
+sed -i -e 's/COMPONENT/'$NAME'/g' $NAME/$NAME.js
 touch $NAME/styles.css
 
 while getopts  "s:m:c" flag; do
@@ -68,13 +68,14 @@ while getopts  "s:m:c" flag; do
           # add import statement to styles.js
           mv $NAME/styles.css $NAME/styles.js
           echo "import styled from 'styled-components';" > $NAME/styles.js
-          # insert import statement for styles into component.jsx
-          sed -i -e "s/import React from 'react';/import React from 'react';\n\nimport {} from '.\/styles.js';/" $NAME/$NAME.jsx
+          # insert import statement for styles into component.js
+          sed -i -e "s/import React from 'react';/import React from 'react';\n\nimport {} from '.\/styles.js';/" $NAME/$NAME.js
         fi
       ;;
     m) echo "-m"
       ;;
     esac
+
 done
 
 
---
+++ templates/styled-component.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const bar = styled.div`
+
+`;
+
+export const foo = styled.div`
+
+`;
